0f7b48e009250a94cf2a9c09f2692fef38676af5
Daniel Sunil Evans <46895409+pebxoxo@users.noreply.github.com>
2020-09-14 11:01:24 +0100

    update2

--- babelGraphic.js
+++ babelGraphic.js
@@ -1,6 +1,7 @@
 var canvas;
 var colMov; 
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'y', 'z', '.', ',', ' ', ' '];
+var pageNo = [];
 var inc = 0;
 var incMov = 1;
 
@@ -17,6 +18,9 @@ function setup() {
   frameRate(1);
   colMov = random(-10, 45);
   
+  for(var i = 0; i < 410; i++){
+      pageNo.push(i+1+'.')
+  }
 }
 
 function draw() {
@@ -24,18 +28,20 @@ function draw() {
  background(0 + colMov);
  noStroke();
  fill(255);
- textSize(windowWidth/100);
+ textSize(windowWidth/120);
 
  for(var i = 0; i < 80; i++){
      for(var j = 0; j < 40; j++){
         var rand = int(random(25));
-        text(letters[rand], (i * windowWidth/80)+10, (j * windowHeight/40)+10)
+        text(letters[rand], (i * (windowWidth-10)/80)+10, (j * (windowHeight-10)/40)+20)
      }
  }
 
-
+textSize(windowWidth/110);
+text(pageNo[inc], 10, 10)
+    
      colMov = random(0, 40)
-        if(inc == 35){
+        if(inc == 410){
          incMov = -1
      }
      
